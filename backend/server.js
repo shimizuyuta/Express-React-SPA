afb168dc602f897ca916defcac0c17bb604b28e6
clean_water <shmizuyuta213@icloud.com>
2021-10-16 11:56:11 +0900

    express-session セットアップ

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 const cors = require('cors')
+const expressSession = require('express-session');
 
-const userRouter = require('./api/users')
+const router = require('./router')
 
 
 
@@ -13,6 +14,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-app.use('/api/user', userRouter)
+const sessionStore = new expressSession.MemoryStore();
+const session = expressSession({
+  store: sessionStore,
+  secret: 'catIsKawaii', // 環境変数で設定などする。今回は省略して固定値
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  proxy: false, // reverse proxy経由などの場合はtrueにする。環境で分けるようにする。今回は省略
+  cookie: {
+    secure: false, // httpsならtrueにする。環境で分けるなどする。今回は省略
+    httpOnly: true,
+    rolling: true,
+    maxAge: 3600000, // ミリ秒で指定。環境変数で設定するべきだが、今回は省略
+  },
+});
+
+app.use(session);
+
+
+
+app.use('/api/v1/', router)
 
 app.listen(port,()=>console.log('start server'))
