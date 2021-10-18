1d051e567a1c4fcf9195138115d1c6bd18319d42
clean_water <shmizuyuta213@icloud.com>
2021-10-18 13:01:07 +0900

    バックエンド　DBセットアップ

--- backend/api/db/dbconnection.js
+++ backend/api/db/dbconnection.js
@@ -1,9 +1,11 @@
 'uses strict'
 const mongoose = require('mongoose')
+require('dotenv').config();
 const url = process.env.MONGO_DB_CONNECTION_URL
+
 module.exports = async() =>{
     try{
-      const conn = await mongoose.connect(process.env.MONGO_DB_CONNECTION_URL,{
+      const conn = await mongoose.connect(url,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useUnifiedTopology: true,
